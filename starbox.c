49ecf6e04f3639aa1fc0ecf40f58593de5e95344
sabavathpraveen7 <sabavathpraveenchinna@gmail.com>
2026-02-03 16:18:54 +0530

    Add files via upload

---
+++ starbox.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int row;
+    int coloum;
+    int i;
+    int j;
+    printf("enter the number of rows \n ");
+    scanf("%d",&row);
+    printf("enter the number of coloum\n");
+    scanf("%d",&coloum);
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=coloum;j++)
+        {
+        if(i==1||i==row||j==1||j==coloum)
+        {
+          printf("*");
+        }
+        else
+        {
+            printf(" ");
+        }
+        }
+        printf("\n");
+        }
+    }
