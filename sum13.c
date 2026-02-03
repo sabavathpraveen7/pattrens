1b21db56c4bdbff146a4397a9ccd344d8abaf3f8
sabavathpraveen7 <sabavathpraveenchinna@gmail.com>
2026-02-03 16:21:32 +0530

    Add files via upload

---
+++ sum13.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+   int size;
+   printf("enter how many element to store \n");
+   scanf("%d",&size);
+   int a[size];
+   for(int i=0;i<=size-1;i++)
+   {
+       printf("enter the %d element \n",(i+1));
+       scanf("%d",&a[i]);
+   }
+   int sum=0;
+    for(int i=0;i<=size-1;i++)
+   if (a[i]!=13)
+   {
+       sum=sum+a[i];
+   }
+   else
+   {
+       break;
+   }
+printf("%d\n is the sum ",sum);
+if(sum==0)
+{
+    printf("-1");
+}
+else
+{
+    printf("%d",sum);
+}
+}
+
