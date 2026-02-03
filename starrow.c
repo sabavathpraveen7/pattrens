d576c9fea43d0cd7b4fb32b9d486bc9649d3a9f6
sabavathpraveen7 <sabavathpraveenchinna@gmail.com>
2026-02-03 16:20:57 +0530

    Add files via upload

---
+++ starrow.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int rows;
+    int i;
+    printf("enter the number of rows \n");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++)
+    {
+        printf("* \n");
+    }
+}
