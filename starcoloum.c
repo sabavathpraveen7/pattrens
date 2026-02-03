fc130f00aaabc90521647839afc3e00ab8bb8aa9
sabavathpraveen7 <sabavathpraveenchinna@gmail.com>
2026-02-03 16:19:39 +0530

    Add files via upload

---
+++ starcoloum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int coloum;
+    int j;
+    printf("enter the number of coloum ");
+    scanf("%d",&coloum);
+    for(j=1;j<=coloum;j++)
+    {
+        printf("*");
+    }
+}
